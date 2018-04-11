bae88ab2c26f29e897942e2646db7dbfe9c41842
akiraandy <andypschwartz@gmail.com>
2018-04-11 09:05:49 -0500

    game now looping properly

--- src/clojure_tic_tac_toe/game_runner.clj
+++ src/clojure_tic_tac_toe/game_runner.clj
@@ -1,7 +1,8 @@
 (ns clojure-tic-tac-toe.game-runner
   (:require [clojure.string :as string]
             [clojure-tic-tac-toe.board :as board]
-            [clojure-tic-tac-toe.messages :as message]))
+            [clojure-tic-tac-toe.messages :as message]
+            [clojure-tic-tac-toe.console-ui :as console]))
 
 (defn get-user-input []
   (string/trim (read-line)))
@@ -10,7 +11,10 @@
   (Integer/parseInt(get-user-input)))
 
 (defn turn [gameboard]
- (board/fill-board gameboard (get-user-input-as-int) :x))
+  (console/print-message(message/display-board gameboard))
+
+  (let [user-input (get-user-input-as-int)]
+ (board/fill-board gameboard user-input :x)))
 
 (defn display-turn [gameboard]
   (message/display-board gameboard)
@@ -18,5 +22,5 @@
 
 (defn game-loop [board]
   (if (board/full? board)
-  board
+    board
   (recur (turn board))))
