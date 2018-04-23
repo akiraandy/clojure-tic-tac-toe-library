0d983bc4df70d220cc39f73cd207d1ca193645f1
akiraandy <andypschwartz@gmail.com>
2018-04-23 10:19:05 -0500

    fix runner bug

--- src/clojure_tic_tac_toe/game_runner.clj
+++ src/clojure_tic_tac_toe/game_runner.clj
@@ -18,8 +18,6 @@
   (recur (turn game-data))))
 
 (defn play-game [game-data]
-  (for [x (range 10)]
-  (console/print-message(messages/clear-screen)))
   (let [end-state (game-loop game-data)]
     (console/print-message(messages/clear-screen))
     (console/print-message(messages/display-board end-state))
